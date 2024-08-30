d787a42e996eca9985bc6432008f5bc46240c1f9
Your Name <you@example.com>
2024-08-30 11:18:55 +0100

    init push

---
+++ src/main/java/com/eduard/ecomerce/model/Product.java
@@ -0,0 +1,30 @@
+package com.eduard.ecomerce.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+@Table(name= "products")
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String description;
+    private String brand;
+    private BigDecimal price;
+    private String category;
+    private Date releaseDate;
+    private boolean productAvailable;
+    private int stockQuantity;
+
+    private String imageName;
+    private String imageType;
+    @Lob
+    private byte[] imageData;
+}
